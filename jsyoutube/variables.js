645636b783c2dea3bde4e7c370447ed778eb66fe
Sakshi0010-creator <sakshishinde.ss0010@gmail.com>
2024-12-30 10:03:43 +0000

    container config

---
+++ jsyoutube/variables.js
@@ -0,0 +1,18 @@
+const accountid= 14453
+var accountpassword = "12345678"
+let accountEmail ="sakshishinde.ss0010@gmail.com"
+accountcity ="jaipur"
+let accountstate; (undefined)
+
+
+
+// accountid= 2 ---not allowed
+// console.log(accountid);
+
+/* preffered not to use var because of the issue in block scope and functional scope*/
+
+accountEmail="hchcch123@gmailc.com"
+accountpassword="12467"
+accountcity="Pune"
+console.table([accountpassword,accountEmail,accountcity,accountstate]);
+
