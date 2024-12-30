021e509c114dcdadee0d6a435f0d327073581104
Sakshi0010-creator <sakshishinde.ss0010@gmail.com>
2024-12-30 10:01:09 +0000

    container config

---
+++ .devcontainer/jsyout/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version//alert(3+3) // we are using nodejs, not browser
+console.log (3
+    +3) // code readability should be high .
+    console.log("sakshi")
+    
+
+    let name = " sakshi" (string)
+let age = 18 
+let isloggedin = false
+
+//number => 2to power 53
+//bigint 
+//string=>""
+//boolean=>true or false
+//null => standalone value also datatype.(empty value)
+//undefined => variable assign but not define value .
+//symbol => unique
+
+
+//Object 
+
+console.log(typeof null); //object 
+console.log(typeof undefine); // undefined 
