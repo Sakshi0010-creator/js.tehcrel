5d8dba0cbdb65163a62daca2d89a5321ca63de10
Sakshi0010-creator <sakshishinde.ss0010@gmail.com>
2024-12-30 11:38:05 +0000

    container config

---
+++ jsyoutube/comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 >  1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+//(basic conversion mostly used!)
+
+console.log("2" > 1); false
+console.log("02" > 1); false
+console.log(null >= 0); true
+//(Avoid this type mostly!!)
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//(Avoid this typeof conversion)
+
+
+//===
+
+console.log("2" == 2);
+console
--- jsyoutube/conversionoperation.js
+++ jsyoutube/conversionoperation.js
@@ -3,7 +3,7 @@ let score = 33
 console.log(typeof score);
 console.loh(typeof (score));
 
-let valueInNumber = Number (score)
+let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
@@ -24,7 +24,41 @@ console.log(booleanILoggedIn);
 //""=> false
 //"sakshi"=>true
 
-let someNumber=33
-let stringNumber= String (someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log( typeof stringNumber);
+
+
+//***************operations***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+let str1 = "hello"
+let str2 = "sakshi"
+let str3 = str1 + str2
+//console.log("str3"); hello sakshi
+
+console.log("1" + 2); 12
+console.log(1 + "2"); 12
+console.log("1" + 2 + 2); 122
+console.log(1 + 2 + "2"); 32
+//don't use like this instead use paranthesis()=>
+console.log((3 + 4) * 5 % 3); //correct way  
+console.log("+true");
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter=100;
+++gamecounter;
+console.log(gamecounter);
+
